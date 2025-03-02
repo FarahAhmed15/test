5c71601eccc7a7d3738d7a06826418844b72434d
Farah Ahmed <faraahahmed155@gmail.com>
2025-03-02 23:39:34 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\CategoryController;
+
+// Ensure that the ProductController class exists in the specified namespace
+// If it does not exist, create the ProductController class in the specified namespace
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+
+});
+
